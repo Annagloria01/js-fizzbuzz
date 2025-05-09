7675fe7427a0e7910abab700b8e364f295ac5cb6
Annagloria <provanorenna@gmail.com>
2025-05-09 23:02:28 +0200

    done exercise

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ console.log("Fizzbuzz")
 // stampare i numeri da 1 a 100//
 for (let i=0; i<100; i++) {
   const num = i + 1
-  console.log('i:', i)
-  console.log('num:', num)
+
+  // sostituire Fizz ai multipli di 3
+  if (num %3==0 && num %5==0) {
+    console.log("FizzBuzz");
+  } else if (num %3==0) {
+    console.log("Fizz");
+  } else if (num %5==0) {
+    console.log("Buzz");
+  } else  {
+    console.log(num)
+  }
 }
 
